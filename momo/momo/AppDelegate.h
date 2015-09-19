61f75b5eec0931bd4298b6f026d7f8998a9a75df
joytear <joytear@163.com>
2015-09-19 23:44:32 +0800

    初始化项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by 刘磊 on 15/9/19.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
